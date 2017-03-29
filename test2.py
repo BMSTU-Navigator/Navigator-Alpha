0a080f626be72d30a42fb6e24b1c7e386f25d0da
alexdark <evtuhov.aleksander@gmail.com>
2017-03-29 10:49:03 +0300

    tests+correct names

--- test2.py
+++ test2.py
@@ -22,8 +22,9 @@ class FloorCheck(unittest.TestCase):
         self.assertEquals(ids_mas,[1,2,7,10,14,16])
 
 
-
-
-
+#
+# Успешно найдена ошибка
+# Не сбрасывается класс Path
+#
 
 unittest.TextTestRunner().run(unittest.defaultTestLoader.loadTestsFromTestCase(FloorCheck))
